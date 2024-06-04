d3a8e5a625e5816781715450d2dd8ba7939a8a47
¨mago¨ <martingramajo08@gmail.com>
2024-06-03 22:02:50 -0300

    py del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="text-center mt-auto">
+    <div className="text-center mt-auto py-4">
       <h6 className="fs-12">
         Rolling Code School © 2024 Todos los derechos reservados
       </h6>
